0d3cbbf89a79a0bfdd2165bf72632357ab54b163
theroboticien <theroboticien@gmail.com>
2022-05-25 17:11:32 +0200

    + updating the verification function to handle expection when there is no data to be verified

--- gasprice.py
+++ gasprice.py
@@ -24,13 +24,17 @@ Main_windows.geometry('300x100')
 Main_windows.title('GasPrice')
 
 #creating a button that launch the download and extract the data
-GasPriceButton=Button(Main_windows,text="Gas Price", width=10,height=2,command=GasPriceDowloadandExtract)
+GasPriceButton=Button(Main_windows,text="Dowload Prices", width=12,height=2,command=GasPriceDowloadandExtract)
 #setting the placement of the createaCSVFile button on int the software window
 GasPriceButton.place(x=200,y=55)
 
+#creating a button that launch the verification process of the downloaded file
+GasPriceButton=Button(Main_windows,text="Verify Price", width=11,height=2,command=GasPriceVerification)
+#setting the placement of the createaCSVFile button on int the software window
+GasPriceButton.place(x=10,y=55)
 
 # exit button
-exit_button = Button(Main_windows,text="Exit",width=10,height=2,command=lambda:Main_windows.quit())
+exit_button = Button(Main_windows,text="Exit",width=12,height=2,command=lambda:Main_windows.quit())
 #setting the placement of the exit button on int the software window
 exit_button.place(x=100,y=55)
 mainloop()
